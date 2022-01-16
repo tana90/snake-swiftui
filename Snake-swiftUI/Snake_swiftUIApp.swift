0744060664ebb04f7ab80510170f1c9e32e8bd8b
TANA <iamtana90@gmail.com>
2022-01-16 15:45:58 +0200

    Initial Commit

---
+++ Snake-swiftUI/Snake_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snake_swiftUIApp.swift
+//  Snake-swiftUI
+//
+//  Created by Tudor Octavian Ana on 16.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Snake_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
